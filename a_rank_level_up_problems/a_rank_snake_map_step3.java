dc954b0d4a96036c1b7e17e6c4701928135194fa
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-09-28 01:12:38 +0900

    rank: C

--- a_rank_level_up_problems/a_rank_snake_map_step3.java
+++ a_rank_level_up_problems/a_rank_snake_map_step3.java
@@ -5,23 +5,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         final int H = sc.nextInt();
         final int W = sc.nextInt();
-        String[][] map = new String[H][W+2];
         String[] line;
       	for(int i = 0; i < H; i++) {
-      	    line = sc.next().split("");     // 行を読み込んで配列に
-      	    map[i][0] = "#";                // 左端を埋める
-      	    for(int j = 1; j <= W; j++) {
-      	        map[i][j] = line[j-1];      // mapに読み込んだ行を代入
+      	    line = sc.next().split("");
+      	    for(int j = 0; j < W; j++) {
+                // 左端または左隣が"#"の場合
+      	        if(j == 0 || line[j-1].equals("#")) {
+                    // 右端または右隣が"#"の場合
+      	            if(j == W-1 || line[j+1].equals("#")) {
+      	                System.out.println(i + " " + j);
+      	            }
+      	        }
       	    }
-        	map[i][W+1] = "#";              // 右端を埋める
-      	}
-      	for(int i = 0; i < H; i++) {
-        	for(int j = 1; j <= W; j++) {
-        	    // 左右が"#"なら座標を出力
-                if(map[i][j-1].equals("#") && map[i][j+1].equals("#")) {
-                    System.out.println(i + " " + (j-1));
-                }
-        	}      		
       	}
     }
 }
