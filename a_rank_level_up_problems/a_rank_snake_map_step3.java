7771aa5940e58c2969df49775fa4f10224b68b88
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-09-28 12:42:36 +0900

    rank: C

--- a_rank_level_up_problems/a_rank_snake_map_step3.java
+++ a_rank_level_up_problems/a_rank_snake_map_step3.java
@@ -5,14 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         final int H = sc.nextInt();
         final int W = sc.nextInt();
-        String[] line;
+        String line;
       	for(int i = 0; i < H; i++) {
-      	    line = sc.next().split("");
+      	    line = sc.next();
       	    for(int j = 0; j < W; j++) {
                 // 左端または左隣が"#"の場合
-      	        if(j == 0 || line[j-1].equals("#")) {
+      	        if(j == 0 || line.charAt(j-1) == '#') {
                     // 右端または右隣が"#"の場合
-      	            if(j == W-1 || line[j+1].equals("#")) {
+      	            if(j == W-1 || line.charAt(j+1) == '#') {
       	                System.out.println(i + " " + j);
       	            }
       	        }
