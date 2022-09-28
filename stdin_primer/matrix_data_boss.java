7af7e1a287642b8198292e15e421c69f3d55bd07
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-09-28 16:51:42 +0900

    Create matrix_data_boss.java

---
+++ stdin_primer/matrix_data_boss.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            int m = sc.nextInt();
+            for(int j = 0; j < m; j++) {
+                System.out.print(sc.nextInt()); 
+                if(j != m-1) {
+                    System.out.print(" "); 
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
