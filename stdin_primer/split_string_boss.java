1f4633f9758d9edc6f1dd71bb6903593f8de7c9e
okadanao <74280232+probabilityhill@users.noreply.github.com>
2022-09-28 16:14:15 +0900

    rank: D

---
+++ stdin_primer/split_string_boss.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] str = "one two three four five".split(" ");
+        for(String s : str) {
+            System.out.println(s);
+        }
+    }
+}
