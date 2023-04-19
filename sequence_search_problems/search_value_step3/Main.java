e55dabaac259e616fc64a43f9b7054ca802d5735
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-04-19 12:56:27 +0900

    Create Main.java

---
+++ sequence_search_problems/search_value_step3/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        final int[] ARRAY = new int[N];
+        for(int i = 0; i < N; i++) {
+            ARRAY[i] = sc.nextInt();
+        }
+        final int K = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            if(ARRAY[i] == K) {
+                System.out.println(i + 1);
+            }
+        }
+    }
+}
