a3b8ffe8c03bcb89b900ce32d0bdedc7eb23c8cf
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-04-20 15:49:15 +0900

    Create Main.java

---
+++ bfs_dfs_problems/maze/Main.java
@@ -0,0 +1,57 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+
+    static class Point {
+        int level, y, x;
+        Point(int level, int y, int x) {
+            this.level = level;
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int H = sc.nextInt();
+        final int W = sc.nextInt();
+        final int Y_START = sc.nextInt();
+        final int X_START = sc.nextInt();
+        final int Y_GOAL = sc.nextInt();
+        final int X_GOAL = sc.nextInt();
+        // グリッドを入力
+        final String[][] GRID = new String[H][W];
+        for (int i = 0; i < H; i++) {
+            GRID[i] = sc.next().split("");
+        }
+        GRID[Y_START][X_START] = "*";
+        // 到達可能なマスを管理するキュー
+        ArrayDeque<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(0, Y_START, X_START));
+        // キューが空になるまで繰り返し
+        String result = "No";
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();  // 先頭の点を取得＆削除
+            // 移動先がゴールであれば終了
+            if(p.y == Y_GOAL && p.x == X_GOAL) {
+                result = String.valueOf(p.level);
+                break;
+            }
+            // 移動先がグリッドの範囲内かつ未到達かつ壁でない場合はキューに追加し到達済みにする
+            for (int i = -1; i <= 1; i += 2) {
+                // 上下の移動
+                if (0 <= p.y + i && p.y + i <= H - 1 && GRID[p.y + i][p.x].equals(".")) {
+                    queue.add(new Point(p.level + 1, p.y + i, p.x));
+                    GRID[p.y + i][p.x] = "*";
+                }
+                // 左右の移動
+                if (0 <= p.x + i && p.x + i <= W - 1 && GRID[p.y][p.x + i].equals(".")) {
+                    queue.add(new Point(p.level + 1, p.y, p.x + i));
+                    GRID[p.y][p.x + i] = "*";
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
