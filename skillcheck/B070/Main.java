27f469cb4eb46bf13c4d2dd72ce169df1d3ab3e4
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-07-22 15:03:57 +0900

    Create Main.java

---
+++ skillcheck/B070/Main.java
@@ -0,0 +1,71 @@
+// 80
+
+import java.util.*;
+
+public class Main {
+    static final int[][] KING_MOVE_AMOUNT = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static final int[][] BISHOP_MOVE_AMOUNT = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
+    // 入力（定数）
+    static final Scanner sc = new Scanner(System.in);
+    static final int N = sc.nextInt();
+    static final int START_ROW_IDX = sc.nextInt() - 1;
+    static final int START_COL_IDX = sc.nextInt() - 1;
+    static final int K = sc.nextInt();
+
+    static int[][] board = new int[N][N];
+    static boolean[][] checked = new boolean[N][N];
+
+    public static void main(String[] args) {
+
+        // 到達可能な場所を1にする
+        board[START_ROW_IDX][START_COL_IDX] = 1;
+        move(START_ROW_IDX, START_COL_IDX, 1);
+
+        int reachableCellCount = 0;
+        for(int[] row : board) {
+            for(int cell : row) {
+                reachableCellCount += cell;
+            }
+        }
+
+        // 出力
+        System.out.println(reachableCellCount);
+    }
+
+    
+    private static void move(int currRowIdx, int currColIdx, int moveCount) {
+        if(moveCount > K) {
+            return;
+        }
+
+        checked[currRowIdx][currColIdx] = true;
+
+        // キングの動きをする
+        for(int[] moveAmount : KING_MOVE_AMOUNT) {
+            int nextRowIdx = currRowIdx + moveAmount[0];
+            int nextColIdx = currColIdx + moveAmount[1];
+            if(canPut(nextRowIdx, nextColIdx)) {
+                board[nextRowIdx][nextColIdx] = 1;
+                move(nextRowIdx, nextColIdx, moveCount + 1);
+            }
+        }
+
+        // ビショップの動きをする
+        for(int[] moveAmount : BISHOP_MOVE_AMOUNT) {
+            int nextRowIdx = currRowIdx + moveAmount[0];
+            int nextColIdx = currColIdx + moveAmount[1];
+            while(canPut(nextRowIdx, nextColIdx)) {
+                board[nextRowIdx][nextColIdx] = 1;
+                move(nextRowIdx, nextColIdx, moveCount + 1);
+                nextRowIdx += moveAmount[0];
+                nextColIdx += moveAmount[1];
+            }
+        }
+    }
+
+    // boardの範囲内かつ未到達かどうか
+    private static boolean canPut(int rowIdx, int colIdx) {
+        return rowIdx >= 0 && rowIdx < N && colIdx >= 0 && colIdx < N && !checked[rowIdx][colIdx];
+    }
+}
