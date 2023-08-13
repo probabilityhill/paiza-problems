b82025abdbe7d63dd832ef921b9e44f9ee2d2c6d
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-14 01:13:00 +0900

    Refactor: 不要な配列削除し簡潔に

--- skillcheck/B025/Main.java
+++ skillcheck/B025/Main.java
@@ -1,6 +1,6 @@
 // 44
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,45 +10,34 @@ public class Main {
         final int K = sc.nextInt();
 
         // 各うさぎの位置
-        final int[] rabbitPos = new int[M];
-        // 各しげみのうさぎ番号
-        final int[] places = new int[N];
-             
-        int pos;
+        List<Integer> rabbitPos = new ArrayList<>();
         for(int ri = 0; ri < M; ri++) {
-            pos = sc.nextInt();
-            rabbitPos[ri] = pos;
-            places[pos - 1] = ri + 1;
+            rabbitPos.add(sc.nextInt() - 1);
         }
 
-        // Kセットジャンプする
+        // Kセットループ
         for(int i = 0; i < K; i++) {
             // うさぎループ
             for(int ri = 0; ri < M; ri++) {
                 // 現在地
-                int currPosIdx = rabbitPos[ri] - 1;
+                int currPos = rabbitPos.get(ri);
 
                 // 空いているしげみを探す
-                int targetPosIdx = 0;
                 for(int pi = 1; pi < N; pi++) {
-                    targetPosIdx = (currPosIdx + pi) % N;
+                    int targetPos = (currPos + pi) % N;
 
-                    // しげみが空いていたら探索終了
-                    if(places[targetPosIdx] == 0) {
+                    // しげみが空いていたらジャンプして終了
+                    if(!rabbitPos.contains(targetPos)) {
+                        rabbitPos.set(ri, targetPos);
                         break;
                     }
                 }
-
-                // ジャンプする
-                places[currPosIdx] = 0;
-                places[targetPosIdx] = ri + 1;
-                rabbitPos[ri] = targetPosIdx + 1;
             }
         }
 
         // うさぎの位置を出力
         for(int p : rabbitPos) {
-            System.out.println(p);
+            System.out.println(p + 1);
         }
     }
 }
