3d1f7d7dc9a775ed372955eb8b092f2234637845
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-14 20:45:06 +0900

    再解答

--- bfs_dfs_problems/length_between_two_vertex/Main.java
+++ bfs_dfs_problems/length_between_two_vertex/Main.java
@@ -1,58 +1,60 @@
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int dist;
-        int idx;
-        Node(int dist, int idx) {
-            this.dist = dist;
-            this.idx = idx;
-        }
-    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final int numNodes = sc.nextInt();
-        final int nodeX = sc.nextInt();
-        final int nodeY = sc.nextInt();
-        // 隣接リスト作成
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-        for(int i = 0; i < numNodes; i++) {
-            adjList.add(new ArrayList<>());
-        }
-        int el, er;
-        for(int i = 0; i < numNodes - 1; i++) {
-            el = sc.nextInt();
-            er = sc.nextInt();
-            adjList.get(el - 1).add(er - 1);
-            adjList.get(er - 1).add(el - 1);
-        }
-        // 訪問済みのチェックを行う配列作成
-        boolean[] visited = new boolean[numNodes];
-        visited[nodeX - 1] = true;
-        // キュー作成
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        // 最初のノードをキューに追加
-        queue.add(new Node(0, nodeX - 1));
+        final int N = sc.nextInt();
+        final int X = sc.nextInt();
+        final int Y = sc.nextInt();
 
-        // 頂点Yが見つかるまで繰り返し
-        int result;
-        while(true) {
-            // 先頭を取得＆キューから削除
-            Node nowNode = queue.poll();
-            // 頂点Yが見つかったら終了
-            if(nowNode.idx == nodeY - 1) {
-                result = nowNode.dist;
+        // 隣接ノードを記録
+        List<List<Integer>> adjNodesList = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            adjNodesList.add(new ArrayList<>());
+        }
+        for(int i = 0; i < N - 1; i++) {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            adjNodesList.get(n1 - 1).add(n2 - 1);
+            adjNodesList.get(n2 - 1).add(n1 - 1);
+        }
+
+        // 訪問管理
+        boolean[] isVisited = new boolean[N];
+
+        // キュー
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(X - 1, 0));
+        isVisited[X - 1] = true;
+
+        while(!queue.isEmpty()) {
+            Node currNode = queue.poll();
+
+            // Yに到達したら距離を出力して終了
+            if(currNode.idx == Y - 1) {
+                System.out.println(currNode.dist);
                 break;
             }
-            // 隣接ノードを取得
-            for(int adjNodeIdx : adjList.get(nowNode.idx)) {
-                // 未訪問であれば隣接ノードをキューに追加し訪問済みにする
-                if(!visited[adjNodeIdx]) {
-                    queue.add(new Node(nowNode.dist + 1, adjNodeIdx));
-                    visited[adjNodeIdx] = true;
+
+            // 未訪問の隣接ノードをキューに追加
+            List<Integer> adjNodes = adjNodesList.get(currNode.idx);
+            for(int nodeIdx : adjNodes) {
+                if(!isVisited[nodeIdx]) {
+                    isVisited[nodeIdx] = true;
+                    queue.add(new Node(nodeIdx, currNode.dist + 1));
                 }
             }
         }
-        System.out.println(result);
+    }
+}
+
+class Node {
+    int idx;
+    int dist;
+
+    Node(int idx, int dist) {
+        this.idx = idx;
+        this.dist = dist;
     }
 }
