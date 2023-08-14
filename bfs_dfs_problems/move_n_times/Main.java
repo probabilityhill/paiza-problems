78bacade230df6c5a01aa7ec511a63f9ad360430
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-14 16:11:54 +0900

    Refactor

--- bfs_dfs_problems/move_n_times/Main.java
+++ bfs_dfs_problems/move_n_times/Main.java
@@ -1,59 +1,73 @@
-import java.util.ArrayDeque;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
-    static class Point {
-        int level, y, x;
-        Point(int level, int y, int x) {
-            this.level = level;
-            this.y = y;
-            this.x = x;
-        }
-    }
+    // 上下左右の移動距離
+    private static final int[][] MOVE_DIST = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static void main(String[] args) {
+        // 入力
         Scanner sc = new Scanner(System.in);
         final int H = sc.nextInt();
         final int W = sc.nextInt();
         final int N = sc.nextInt();
-        final int Y_START = sc.nextInt();
-        final int X_START = sc.nextInt();
-        // グリッドを初期化
-        final String[][] GRID = new String[H][W];
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                GRID[i][j] = ".";
+        final int y = sc.nextInt();
+        final int x = sc.nextInt();
+
+        // 壁以外trueにする
+        boolean[][] grid = new boolean[H + 2][W + 2];
+        for(int i = 0; i < H; i++) {
+            for(int j = 0; j < W; j++) {
+                grid[i + 1][j + 1] = true;
             }
-        }
-        GRID[Y_START][X_START] = "*";
-        // 到達可能なマスを管理するキュー
-        ArrayDeque<Point> queue = new ArrayDeque<>();
-        queue.add(new Point(0, Y_START, X_START));
-        // キューが空になるまで繰り返し
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();  // 先頭の点を取得＆削除
-            // 深さNであれば移動しない
-            if (p.level >= N) {
+        }        
+        
+        // キュー作成
+        Deque<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(y, x, 0));
+        grid[y + 1][x + 1] = false;
+
+        // 幅優先探索
+        while(!queue.isEmpty()) {
+            Point currPoint = queue.poll();
+
+            // 深さの上限に達したらはスキップ
+            if(currPoint.depth >= N) {
                 continue;
-            }
-            // 移動先がグリッドの範囲内かつ未到達の場合はキューに追加し到達済みにする
-            for (int i = -1; i <= 1; i += 2) {
-                // 上下の移動
-                if (0 <= p.y + i && p.y + i <= H - 1 && GRID[p.y + i][p.x].equals(".")) {
-                    queue.add(new Point(p.level + 1, p.y + i, p.x));
-                    GRID[p.y + i][p.x] = "*";
-                }
-                // 左右の移動
-                if (0 <= p.x + i && p.x + i <= W - 1 && GRID[p.y][p.x + i].equals(".")) {
-                    queue.add(new Point(p.level + 1, p.y, p.x + i));
-                    GRID[p.y][p.x + i] = "*";
+            }            
+
+            // 1回の移動で到達可能なマスをキューに追加
+            for(int[] dist : MOVE_DIST) {
+                int nextPointY = currPoint.y + dist[0];
+                int nextPointX = currPoint.x + dist[1];
+                // 未到達の場合キューに追加し到達済みにする
+                if(grid[nextPointY + 1][nextPointX + 1]) {
+                    queue.add(new Point(nextPointY, nextPointX, currPoint.depth + 1));
+                    grid[nextPointY + 1][nextPointX + 1] = false;
                 }
             }
         }
 
-        for (int i = 0; i < H; i++) {
-            System.out.println(String.join("", GRID[i]));
+        // グリッドを出力（到達済みはfalse）
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < H; i++) {
+            for(int j = 0; j < W; j++) {
+                sb.append(grid[i + 1][j + 1] ? "." : "*");
+            }
+            sb.append("\n");
         }
+        System.out.println(sb.toString());
+    }
+}
+
+// 座標クラス
+class Point {
+    int y;
+    int x;
+    int depth;
+
+    Point(int y, int x, int d) {
+        this.y = y;
+        this.x = x;
+        this.depth = d;
     }
 }
