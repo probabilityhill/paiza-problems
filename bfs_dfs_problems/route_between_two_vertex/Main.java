b4a8aa390abca3add5e3e620a09cbc4b0d2c9323
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-14 21:51:15 +0900

    再解答

--- bfs_dfs_problems/route_between_two_vertex/Main.java
+++ bfs_dfs_problems/route_between_two_vertex/Main.java
@@ -1,66 +1,66 @@
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int idx;
-        ArrayList<Integer> route;
-        Node(int idx, ArrayList<Integer> route) {
-            this.idx = idx;
-            this.route = route;
-        }
-    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final int numNodes = sc.nextInt();
-        final int nodeX = sc.nextInt();
-        final int nodeY = sc.nextInt();
-        // 隣接リスト作成
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-        for(int i = 0; i < numNodes; i++) {
-            adjList.add(new ArrayList<>());
-        }
-        int el, er;
-        for(int i = 0; i < numNodes - 1; i++) {
-            el = sc.nextInt();
-            er = sc.nextInt();
-            adjList.get(el - 1).add(er - 1);
-            adjList.get(er - 1).add(el - 1);
-        }
-        // 訪問済みのチェックを行う配列作成
-        boolean[] visited = new boolean[numNodes];
-        visited[nodeX - 1] = true;
-        // キュー作成
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-        // 最初のノードをキューに追加
-        queue.add(new Node(nodeX - 1, new ArrayList<>(List.of(nodeX - 1))));
+        final int N = sc.nextInt();
+        final int X = sc.nextInt();
+        final int Y = sc.nextInt();
 
-        // 頂点Yが見つかるまで繰り返し
-        ArrayList<Integer> ansRoute;
-        while(true) {
-            // 先頭を取得＆キューから削除
-            Node nowNode = queue.poll();
-            // 頂点Yが見つかったら終了
-            if(nowNode.idx == nodeY - 1) {
-                ansRoute = nowNode.route;
+        // 隣接ノードを記録
+        List<List<Integer>> adjNodesList = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            adjNodesList.add(new ArrayList<>());
+        }
+        for(int i = 0; i < N - 1; i++) {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            adjNodesList.get(n1 - 1).add(n2 - 1);
+            adjNodesList.get(n2 - 1).add(n1 - 1);
+        }
+
+        // 訪問管理
+        boolean[] isVisited = new boolean[N];
+
+        // キュー
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(X - 1, 0, Arrays.asList(X - 1)));
+        isVisited[X - 1] = true;
+
+        while(!queue.isEmpty()) {
+            Node currNode = queue.poll();
+
+            // Yに到達したら経路を出力して終了
+            if(currNode.idx == Y - 1) {
+                for(Integer nodeIdx : currNode.route) {
+                    System.out.println(nodeIdx + 1);
+                }
                 break;
             }
-            // 隣接ノードを取得
-            for(int adjNodeIdx : adjList.get(nowNode.idx)) {
-                // 未訪問であれば隣接ノードに経路を記録しキューに追加
-                if(!visited[adjNodeIdx]) {
-                    // 今までの経路を取得し隣接ノードに記録
-                    ArrayList<Integer> routeHistory =  new ArrayList<>(nowNode.route);
-                    Node adjNode = new Node(adjNodeIdx, routeHistory);
-                    // 経路に隣接ノードを追加
-                    adjNode.route.add(adjNodeIdx);
+
+            // 未訪問の隣接ノードをキューに追加
+            List<Integer> adjNodes = adjNodesList.get(currNode.idx);
+            for(int nodeIdx : adjNodes) {
+                if(!isVisited[nodeIdx]) {
+                    isVisited[nodeIdx] = true;
+                    Node adjNode = new Node(nodeIdx, currNode.dist + 1, new ArrayList<>(currNode.route));
+                    adjNode.route.add(adjNode.idx);
                     queue.add(adjNode);
-                    // 訪問済みにする
-                    visited[adjNodeIdx] = true;
                 }
             }
         }
-        for(int nodeIdx : ansRoute) {
-            System.out.println(nodeIdx + 1);
-        }
+    }
+}
+
+class Node {
+    int idx;
+    int dist;
+    List<Integer> route;
+
+    Node(int idx, int dist, List<Integer> route) {
+        this.idx = idx;
+        this.dist = dist;
+        this.route = route;
     }
 }
