d1872a9f89d0b7d83554ed9c477ca3e03d60cc82
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-14 16:52:16 +0900

    Refactor

--- bfs_dfs_problems/judge_reach/Main.java
+++ bfs_dfs_problems/judge_reach/Main.java
@@ -1,58 +1,76 @@
-import java.util.ArrayDeque;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    private static final int WALL = 0;
+    private static final int ROAD = 1;
+    private static final int REACHED = 2;
+    private static final char[] MARKS = {'#', '.', '*'};
 
-    static class Point {
-        int level, y, x;
-        Point(int level, int y, int x) {
-            this.level = level;
-            this.y = y;
-            this.x = x;
-        }
-    }
+    // 上下左右の移動距離
+    private static final int[][] MOVE_DIST = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static void main(String[] args) {
+        // 入力
         Scanner sc = new Scanner(System.in);
         final int H = sc.nextInt();
         final int W = sc.nextInt();
-        final int Y_START = sc.nextInt();
-        final int X_START = sc.nextInt();
-        final int Y_GOAL = sc.nextInt();
-        final int X_GOAL = sc.nextInt();
-        // グリッドを入力
-        final String[][] GRID = new String[H][W];
-        for (int i = 0; i < H; i++) {
-            GRID[i] = sc.next().split("");
-        }
-        GRID[Y_START][X_START] = "*";
-        // 到達可能なマスを管理するキュー
-        ArrayDeque<Point> queue = new ArrayDeque<>();
-        queue.add(new Point(0, Y_START, X_START));
-        // キューが空になるまで繰り返し
-        String result = "No";
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();  // 先頭の点を取得＆削除
-            // 移動先がゴールであれば終了
-            if(p.y == Y_GOAL && p.x == X_GOAL) {
-                result = "Yes";
-                break;
+        final int sy = sc.nextInt();
+        final int sx = sc.nextInt();
+        final int gy = sc.nextInt();
+        final int gx = sc.nextInt();
+
+        boolean isGoalReached = false;
+
+        // 迷路
+        int[][] grid = new int[H + 2][W + 2];
+        for(int i = 0; i < H; i++) {
+            String row = sc.next();
+            for(int j = 0; j < W; j++) {
+                grid[i + 1][j + 1] = row.charAt(j) == MARKS[ROAD] ? ROAD : WALL;
             }
-            // 移動先がグリッドの範囲内かつ未到達かつ壁でない場合はキューに追加し到達済みにする
-            for (int i = -1; i <= 1; i += 2) {
-                // 上下の移動
-                if (0 <= p.y + i && p.y + i <= H - 1 && GRID[p.y + i][p.x].equals(".")) {
-                    queue.add(new Point(p.level + 1, p.y + i, p.x));
-                    GRID[p.y + i][p.x] = "*";
-                }
-                // 左右の移動
-                if (0 <= p.x + i && p.x + i <= W - 1 && GRID[p.y][p.x + i].equals(".")) {
-                    queue.add(new Point(p.level + 1, p.y, p.x + i));
-                    GRID[p.y][p.x + i] = "*";
+        }
+        
+        // キュー作成
+        Deque<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(sy, sx, 0));
+        grid[sy + 1][sx + 1] = REACHED;
+
+        // 幅優先探索
+        while(!queue.isEmpty()) {
+            Point currPoint = queue.poll();
+
+            // ゴールに達したら終了
+            if(currPoint.y == gy && currPoint.x == gx) {
+                isGoalReached = true;
+                break;
+            }            
+
+            // 1回の移動で到達可能なマスをキューに追加
+            for(int[] dist : MOVE_DIST) {
+                int nextPointY = currPoint.y + dist[0];
+                int nextPointX = currPoint.x + dist[1];
+                // 未到達かつ壁でない場合キューに追加し到達済みにする
+                if(grid[nextPointY + 1][nextPointX + 1] == ROAD) {
+                    queue.add(new Point(nextPointY, nextPointX, currPoint.depth + 1));
+                    grid[nextPointY + 1][nextPointX + 1] = REACHED;
                 }
             }
         }
 
-        System.out.println(result);
+        // ゴールしたかどうかを出力
+        System.out.println(isGoalReached ? "Yes" : "No");
+    }
+}
+
+// 座標クラス
+class Point {
+    int y;
+    int x;
+    int depth;
+
+    Point(int y, int x, int d) {
+        this.y = y;
+        this.x = x;
+        this.depth = d;
     }
 }
