37eeb9f0b63470da802c6948a069abeaea099ff5
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-16 12:17:41 +0900

    再解答

--- skillcheck/C031/Main.java
+++ skillcheck/C031/Main.java
@@ -2,28 +2,37 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // 入力
         Scanner sc = new Scanner(System.in);
         final int N = sc.nextInt();
-        Map<String, Integer> list = new LinkedHashMap<>();
-        for (int i = 0; i < N; i++) {
-            list.put(sc.next(), sc.nextInt());
+
+        // 時差のマップ
+        final Map<String, Integer> timeDiffs = new LinkedHashMap<>();
+        for(int i = 0; i < N; i++) {
+            timeDiffs.put(sc.next(), sc.nextInt());
         }
-        final String PLACE = sc.next();
-        String[] TIME = sc.next().split(":"); 
-        final int H = Integer.parseInt(TIME[0]);
-        final int M = Integer.parseInt(TIME[1]);
-        int diffHour = H - list.get(PLACE);
-        int hour;
-        for (Integer n : list.values()) {
-            hour = diffHour + n;
-            if (hour < 0) {
-                hour += 24;
-            }
-            else if (hour >= 24) {
-                hour -= 24;
-            }
-            System.out.println(String.format("%02d:%02d", hour, M));
-        }
+
+        // 基準となる国の情報
+        final String baseCountry = sc.next();
+        final String[] baseTime = sc.next().split(":");
+        final int baseHours = Integer.parseInt(baseTime[0]);
+        final int baseMinutes = Integer.parseInt(baseTime[1]);
         
+        // ロンドンの時間
+        final int londonHours = baseHours - timeDiffs.get(baseCountry);
+
+        // 各地の時刻を計算し出力
+        for(int diff : timeDiffs.values()) {
+            int hours = londonHours + diff;
+            // 24時間制になるように調整
+            if(hours < 0 ) {
+                hours += 24;
+            }
+            else if(hours >= 24) {
+                hours -= 24;
+            }
+            // hh:mm
+            System.out.println(String.format("%02d:%02d", hours, baseMinutes));
+        }
     }
 }
