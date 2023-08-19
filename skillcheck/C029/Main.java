6580569ea4c1534097e35759538d2f9a16df199a
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-08-20 01:52:40 +0900

    再解答

--- skillcheck/C029/Main.java
+++ skillcheck/C029/Main.java
@@ -1,30 +1,46 @@
+// 18
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final int NUM_HOLIDAY = sc.nextInt();             // 連休の日数
-        final int DAYS_TRAVEL = sc.nextInt();             // 旅行の日数
-        final int[][] WEATHER = new int[NUM_HOLIDAY][2];  // 天気予報
-        for (int i = 0; i < NUM_HOLIDAY; i++) {
-            WEATHER[i][0] = sc.nextInt();
-            WEATHER[i][1] = sc.nextInt();
-        }
-        int s = 0;                                        // 旅行開始日
-        int sum;                                          // 降水確率の合計
-        int minSum = 10000;                               // 降水確率の合計の最小値
-        for (int i = 0; i < NUM_HOLIDAY - DAYS_TRAVEL + 1; i++) {
-            sum = 0;
-            // 旅行期間の降水確率の合計を求める
-            for (int j = 0; j < DAYS_TRAVEL; j++) {
-                sum += WEATHER[i + j][1];
+        // 連休の日数
+        final int totalHolidays = sc.nextInt();
+        // 旅行の日数
+        final int totalTravelDays = sc.nextInt();
+
+        // 降水確率を記録する配列
+        int[] chanceOfRainArray = new int[totalHolidays];
+        // 連休の初日
+        int firstHoliday = 0;
+
+        int day, chanceOfRain;
+        for(int i = 0; i < totalHolidays; i++) {
+            day = sc.nextInt();
+            chanceOfRain = sc.nextInt();
+            // 連休の初日を記録
+            if(i == 0) {
+                firstHoliday = day;
             }
-            // 降水確率の合計が最も小さいとき最小値と開始日を更新
-            if (sum < minSum) {
-                minSum = sum;
-                s = WEATHER[i][0];
-            }
+            chanceOfRainArray[i] = chanceOfRain;
         }
-        System.out.println(s + " " + (s + DAYS_TRAVEL - 1));
+
+        // 降水確率の平均が最小 <=> 合計が最小
+        // 降水確率の合計を求める
+        List<Integer> sumList = new ArrayList<>();
+        for(int i = 0; i < totalHolidays - totalTravelDays + 1; i++) {
+            int sum = 0;
+            for(int j = 0; j < totalTravelDays; j++) {
+                sum += chanceOfRainArray[i + j];
+            }
+            sumList.add(sum);
+        }
+        
+        int minSumIdx = sumList.indexOf(Collections.min(sumList));
+        int firstTravelDay = firstHoliday + minSumIdx;
+        int lastTravelDay = firstTravelDay + totalTravelDays - 1;
+
+        System.out.println(firstTravelDay + " " + lastTravelDay);
     }
 }
