f13e02d5c30807386ee2f09e9827989bbc84f335
okadanao <74280232+probabilityhill@users.noreply.github.com>
2024-01-27 17:42:35 +0900

    一つのマップで記録を管理するように修正

--- skillcheck/B035/Main.java
+++ skillcheck/B035/Main.java
@@ -23,77 +23,68 @@ public class Main {
             // 成績表に表示される上位の人数
             final int topNum = sc.nextInt();
 
-            // 先月の部員全員のジョギング成績
-            final List<Record> lastMonthRecordList = new ArrayList<>();
+            // 記録マップ（key:名前, value:記録）
+            final Map<String, Record> recordMap = new HashMap<>();
+
+            // 先月の部員全員のジョギング成績を記録
             for (int i = 0; i < memberNum; i++) {
                 final String name = sc.next();
                 final int dist = sc.nextInt();
-                lastMonthRecordList.add(new Record(name, dist));
+                recordMap.put(name, new Record(name, dist));
             }
 
-            // 今月のジョギング記録（key:名前, value:記録）
-            final Map<String, Record> thisMonthRecordMap = new HashMap<>();
+            // 今月のジョギング記録を記録マップに追加
             for (int i = 0; i < thisMonthRecordNum; i++) {
                 // 日付は読み飛ばす
                 sc.next();
                 final String name = sc.next();
                 final int dist = sc.nextInt();
-                // Mapに記録を取得（存在しない場合は記録を作成する）
-                Record record = thisMonthRecordMap.getOrDefault(name, new Record(name, 0));
+                // Mapから記録を取得
+                Record record = recordMap.get(name);
                 // 入力された距離を足す
-                record.addDist(dist);
+                record.addThisMonthDist(dist);
                 // 記録を更新する
-                thisMonthRecordMap.put(name, record);
+                recordMap.put(name, record);
             }
 
-            // 先月の成績を距離の降順でソート
-            final List<Record> sortedLastMonthRecordList = lastMonthRecordList.stream()
-                .sorted(Comparator.comparing(Record::getDist).reversed()).collect(Collectors.toList());
+            // 記録を先月の距離の降順でソート
+            final List<Record> sortedLastMonthRecordList = new ArrayList<>(recordMap.values()).stream()
+                .sorted(Comparator.comparing(Record::getLastMonthDist).reversed()).collect(Collectors.toList());
 
-            // 先月のTOP（key:名前, value:順位(0-based)）
-            final Map<String, Integer> lastMonthTop = new HashMap<>();
-            // 先月の成績から先月のTOPのMap作成
-            for (int rank = 0; rank < topNum; rank++) {
-                final String name = sortedLastMonthRecordList.get(rank).getName();
-                lastMonthTop.put(name, rank);
+            // 先月の順位を記録
+            for (int rank = 1; rank <= topNum; rank++) {
+                final String name = sortedLastMonthRecordList.get(rank - 1).getName();
+                // Mapから記録を取得
+                Record record = recordMap.get(name);
+                // 先月の順位を記録
+                record.setLastMonthRank(rank);
+                // 記録を更新する
+                recordMap.put(name, record);
             }
 
-            // 今月のジョギング記録(Map)をListに変換
-            final List<Record> thisMonthRecordList = new ArrayList<>(thisMonthRecordMap.values());
-            // 先月のジョギング成績から今月走っていない人を探し記録に追加する
-            for (Record record : lastMonthRecordList) {
-                final String name = record.getName();
-                if (!thisMonthRecordMap.containsKey(name)) {
-                    thisMonthRecordList.add(new Record(name, 0));
-                }
-            }
-
-            // 今月のジョギング記録を距離の降順、名前の昇順でソート
-            final List<Record> sortedthisMonthRecordList = thisMonthRecordList.stream()
-                .sorted(Comparator.comparing(Record::getDist).reversed().thenComparing(Record::getName)).collect(Collectors.toList());
+            // 記録を今月の距離の降順、名前の昇順でソート
+            final List<Record> sortedthisMonthRecordList = new ArrayList<>(recordMap.values()).stream()
+                .sorted(Comparator.comparing(Record::getThisMonthDist).reversed().thenComparing(Record::getName)).collect(Collectors.toList());
 
             // 今月のTOP
             StringBuilder thisMonthTop = new StringBuilder();
-            // 今月の成績をから今月のTOPを求める
-            for (int rank = 0; rank < topNum; rank++) {
-                final Record record = sortedthisMonthRecordList.get(rank);
+            // 今月の成績から今月のTOPを求める
+            for (int rank = 1; rank <= topNum; rank++) {
+                final Record record = sortedthisMonthRecordList.get(rank - 1);
                 final String name = record.getName();
-                final int dist = record.getDist();
+                final int dist = record.getThisMonthDist();
+                final int lastMonthRank = record.getLastMonthRank();
                 // 先月のTOPを参照し今月のTOPのラベルを設定する
                 String label = "";
                 // 先月のTOPに入っていない場合
-                if (!lastMonthTop.containsKey(name)) {
+                if (lastMonthRank == 0) {
                     label = LABEL.NEW;
+                } else if (rank < lastMonthRank) {
+                    label = LABEL.UP;
+                } else if (rank == lastMonthRank) {
+                    label = LABEL.SAME;
                 } else {
-                    // 先月の順位
-                    final int lastMonthRank = lastMonthTop.get(name);
-                    if (rank < lastMonthRank) {
-                        label = LABEL.UP;
-                    } else if (rank == lastMonthRank) {
-                        label = LABEL.SAME;
-                    } else {
-                        label = LABEL.DOWN;
-                    }
+                    label = LABEL.DOWN;
                 }
                 thisMonthTop.append(name + " " + dist + " " + label + "\n");
             }
@@ -107,22 +98,36 @@ public class Main {
 // 記録クラス
 class Record {
     private String name;
-    private int dist;
+    private int lastMonthDist;
+    private int lastMonthRank;
+    private int thisMonthDist;
 
-    Record(String name, int dist) {
+    Record(String name, int lastMonthDist) {
         this.name = name;
-        this.dist = dist;
+        this.lastMonthDist = lastMonthDist;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getDist() {
-        return dist;
+    public int getLastMonthDist() {
+        return lastMonthDist;
     }
 
-    public void addDist(int dist) {
-        this.dist += dist;
+    public int getLastMonthRank() {
+        return lastMonthRank;
+    }
+
+    public int getThisMonthDist() {
+        return thisMonthDist;
+    }
+
+    public void addThisMonthDist(int dist) {
+        this.thisMonthDist += dist;
+    }
+
+    public void setLastMonthRank(int rank) {
+        this.lastMonthRank = rank;
     }
 }
