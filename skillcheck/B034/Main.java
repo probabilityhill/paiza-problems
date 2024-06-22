e91c06e0c7a20b1f9601839e2c8db232af0e2e00
minoru tanaka <74280232+probabilityhill@users.noreply.github.com>
2024-06-22 21:43:32 +0900

    refactor: Robotクラスを作成

--- skillcheck/B034/Main.java
+++ skillcheck/B034/Main.java
@@ -23,29 +23,55 @@ public class Main {
 
             // 命令の数
             final int N = sc.nextInt();
-            // 現在地
-            Point currPoint = new Point(startX, startY);
-            // 現在向いている方向のインデックス
-            int currDirIdx = 0;
+            // ロボットを開始地点に配置
+            Robot robot = new Robot(new Point(startX, startY), 0);
             for (int i = 0; i < N; i++) {
                 String command = sc.next();
                 String dir = sc.next();
 
-                // 命令された方向のインデックス（相対的な方向）
-                int relativeDirIdx = DIRECTIONS.indexOf(dir);
-                // 現在向いている方向を考慮した方向のインデックス（絶対的な方向）
-                int dirIdx = (currDirIdx + relativeDirIdx) % DIRECTIONS.size();
-
-                if (MOVE.equals(command)) {
-                    // 指定された方向に進める距離だけ進む
-                    currPoint.move(DIR_POINTS[dirIdx], dists[relativeDirIdx]);
-                } else {
-                    // 方向転換する
-                    currDirIdx = dirIdx;
-                }
+                // 命令に従って動く
+                robot.action(command, dir, dists);
             }
             // 到達地点を出力
-            System.out.println(currPoint.getX() + " " + currPoint.getY());
+            System.out.println(robot.getPos().getX() + " " + robot.getPos().getY());
+        }
+    }
+
+    static class Robot {
+        // 現在地
+        private Point pos;
+        // 現在向いている方向
+        private int dirIdx;
+
+        Robot(Point pos, int dirIdx) {
+            this.pos = pos;
+            this.dirIdx = dirIdx;
+        }
+
+        public Point getPos() {
+            return this.pos;
+        }
+
+        public int getDirIdx() {
+            return this.dirIdx;
+        }
+
+        /*
+         * 命令に従って動く
+         */
+        public void action(String command, String commandDir, int[] dists) {
+            // 命令された方向のインデックス（相対的な方向）
+            int relativeCommandDirIdx = DIRECTIONS.indexOf(commandDir);
+            // 現在向いている方向を考慮した方向のインデックス（絶対的な方向）
+            int commandDirIdx = (this.dirIdx + relativeCommandDirIdx) % DIRECTIONS.size();
+
+            if (MOVE.equals(command)) {
+                // 指定された方向に進める距離だけ進む
+                pos.move(DIR_POINTS[commandDirIdx], dists[relativeCommandDirIdx]);
+            } else {
+                // 方向転換する
+                this.dirIdx = commandDirIdx;
+            }
         }
     }
 }
