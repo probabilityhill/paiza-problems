e01471bfbdd36a1532fcabae5e23a71887211e72
minoru tanaka <74280232+probabilityhill@users.noreply.github.com>
2025-03-22 22:33:32 +0900

    Create Main.java

---
+++ dp_primer/dp_primer_apples_boss/Main.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            final int n = sc.nextInt();
+            final int x = sc.nextInt();
+            final int a = sc.nextInt();
+            final int y = sc.nextInt();
+            final int b = sc.nextInt();
+            final int z = sc.nextInt();
+            final int c = sc.nextInt();
+
+            // dp[n]: ちょうど n 個のりんごを買うのに必要な金額の最小値
+            // dp[k] = min(dp[k-x] + a, dp[k-y] + b, dp[k-z] + c)
+            // 買ったりんごがn+1個以上になってもよい場合
+            // dp[n] = min(dp[n], dp[n+1], ...)
+
+            int[] dp = new int[n + z];
+            Arrays.fill(dp, 10000000);
+            dp[0] = 0;
+            for (int i = 1; i < dp.length; i++) {
+                if (i >= x) {
+                    dp[i] = Math.min(dp[i], dp[i - x] + a);
+                }
+                if (i >= y) {
+                    dp[i] = Math.min(dp[i], dp[i - y] + b);
+                }
+                if (i >= z) {
+                    dp[i] = Math.min(dp[i], dp[i - z] + c);
+                }
+            }
+
+            int minValue = Arrays.stream(dp, n, dp.length).min().getAsInt();
+            System.out.println(minValue);
+        }
+    }
+}
